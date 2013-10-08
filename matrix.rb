7332496d221e4018c059575316118c234e2e2321
Francisco Mendoza <alu0100600582@ull.edu.es>
2013-10-08 13:01:30 +0100

    Suma añadida

--- matrix.rb
+++ matrix.rb
@@ -1,6 +1,7 @@
 #Suma de dos matrices
 
-#! /usr/bin/ruby	
+#! /usr/local/rvm/rubies/ruby-2.0.0-p247/bin/ruby
+	
 
 a = [[2,4,2],[1,6,0],[4,2,3]]  #matriz a
 b = [[0,2,3],[5,3,0],[2,2,2]]  #matriz b
@@ -32,6 +33,31 @@ b.each do |fila|
 end
 
 puts "\n La Suma de la Matriz a y b es ="
+c.each do |fila|
+  puts fila.join(" ")
+end
+
+
+#Multiplicacion de dos matrices
+
+
+for i in 0...m do
+  c[i]=Array.new
+    for j in 0...n do
+      c[i][j] = 0
+    end
+end
+
+for i in 0...m do
+  for j in 0...n do
+    for z in 0...m do
+      c[i][j] += a[i][z] + b[z][j]
+     end
+   end
+ end  
+
+
+puts "\n La Multiplicacion de la Matriz a y b es ="
 c.each do |fila|
   puts fila.join(" ")
 end
